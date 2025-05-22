1ad444cad064b0ae3c8e5d4747be01512177dee4
Antonio <antoniojacintoa@gmail.com>
2025-05-22 19:31:19 -0300

    ajustando menu hamburguer

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,16 @@
 
 const hamburguer = document.querySelector('.hamburguer');
 const headerMenu = document.querySelector('.header-menu');
+const pagina = document.body
 
 function toggleMenu(){
     hamburguer.classList.toggle('active');
     headerMenu.classList.toggle('active');
+    if (hamburguer.classList.contains('active')) {
+        pagina.style.overflowY = 'hidden'
+    } else {
+        pagina.style.overflowY = 'auto'
+    }
 }
 hamburguer.addEventListener('click', toggleMenu);
+
